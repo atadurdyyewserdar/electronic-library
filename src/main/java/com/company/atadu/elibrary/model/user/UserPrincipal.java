1d4bb8bb3f39d41d4d3b0bfd53b452c367649a26
atadurdyyewserdar <atadurdyyewserdar@gmail.com>
2021-12-02 00:34:06 +0300

    User's principal class added for implementing spring security

---
+++ src/main/java/com/company/atadu/elibrary/model/user/UserPrincipal.java
@@ -0,0 +1,57 @@
+package com.company.atadu.elibrary.model.user;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserPrincipal implements UserDetails {
+
+    private AppUser appUser;
+
+    public UserPrincipal(AppUser appUser) {
+        this.appUser = appUser;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.stream(this.appUser.getAuthorities())
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return this.appUser.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.appUser.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.appUser.isNotLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.appUser.isActive();
+    }
+}
