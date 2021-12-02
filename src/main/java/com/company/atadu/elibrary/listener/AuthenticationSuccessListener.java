36bc7060d02985c441c6ba034657aaa115bf5f46
atadurdyyewserdar <atadurdyyewserdar@gmail.com>
2021-12-02 20:47:47 +0300

    Authentication listeners. (Failure and Success)

---
+++ src/main/java/com/company/atadu/elibrary/listener/AuthenticationSuccessListener.java
@@ -0,0 +1,27 @@
+package com.company.atadu.elibrary.listener;
+
+import com.company.atadu.elibrary.model.user.UserPrincipal;
+import com.company.atadu.elibrary.service.user.LoginAttemptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationSuccessListener {
+    private LoginAttemptService loginAttemptService;
+
+    @Autowired
+    public AuthenticationSuccessListener(LoginAttemptService loginAttemptService) {
+        this.loginAttemptService = loginAttemptService;
+    }
+
+    @EventListener
+    public void onAuthenticationSuccess(AuthenticationSuccessEvent event) {
+        Object principal = event.getAuthentication().getPrincipal();
+        if (principal instanceof UserPrincipal) {
+            UserPrincipal user = (UserPrincipal) event.getAuthentication().getPrincipal();
+            loginAttemptService.evictUserFromLoginAttemptCache(user.getUsername());
+        }
+    }
+}
