8676c3abd9db60a83556a091fc100607497c7da0
atadurdyyewserdar <atadurdyyewserdar@gmail.com>
2021-12-02 20:50:29 +0300

    Exceptions added

---
+++ src/main/java/com/company/atadu/elibrary/exception/EmailExistException.java
@@ -0,0 +1,7 @@
+package com.company.atadu.elibrary.exception;
+
+public class EmailExistException extends Exception {
+    public EmailExistException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/company/atadu/elibrary/exception/UsernameExistException.java
@@ -0,0 +1,7 @@
+package com.company.atadu.elibrary.exception;
+
+public class UsernameExistException extends Exception {
+    public UsernameExistException(String message) {
+        super(message);
+    }
+}
