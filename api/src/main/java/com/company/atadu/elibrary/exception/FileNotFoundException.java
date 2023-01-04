19b0e2ef9d8f8c10aa6491083294397d844e2a96
Serdar <atadurdyyewserdar@gmail.com>
2023-01-05 00:35:32 +0300

    New Exception classes

---
+++ api/src/main/java/com/company/atadu/elibrary/exception/FileNotFoundException.java
@@ -0,0 +1,7 @@
+package com.company.atadu.elibrary.exception;
+
+public class FileNotFoundException extends Exception{
+    public FileNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ api/src/main/java/com/company/atadu/elibrary/exception/InvalidCredentialsException.java
@@ -0,0 +1,7 @@
+package com.company.atadu.elibrary.exception;
+
+public class InvalidCredentialsException extends Exception {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
